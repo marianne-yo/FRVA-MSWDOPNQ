7f8d82bd6b322e6b2f36e03679cb21a42aa04f4b
marianne-yo <mbalen39@gmail.com>
2026-02-13 20:38:36 +0800

    updated the UI

--- app/admin/responses/page.tsx
+++ app/admin/responses/page.tsx
@@ -1,20 +1,76 @@
+"use client"
+
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+import { Table, TableHeader, TableBody, TableHead, TableFooter, TableRow, TableCell, TableCaption } from "@/components/ui/table"
+import data from "../../lib/data.json"
+
+import { useEffect, useState } from "react"
+
 export default function Response(){
+    const [searchTerm, setSearchTerm] = useState('');
+    const [results, setResults] = useState(data);
+
+    const handleSearch = (e) => {
+        const term = e.target.value;
+        setSearchTerm(term);
+        if(term === ''){
+            setResults(data);
+        } else{
+                const filteredData = data.filter((item) =>
+                item.name.toLowerCase().includes(searchTerm.toLowerCase())
+            );
+            setResults(filteredData);        
+        }
+    }
+
+
     return(
         <div>
             <main>
                 <h1 className="font-black text-3xl py-5 px-2">RESPONSES</h1>
                 <div className="p-3 w-full flex flex-col justify-start">
-                    <form action="get">
-                        <label htmlFor="search" className="font-medium text-1xl lg:text-2xl">Search</label> <br />
-                        <input type="search" name="" id="" className="bg-slate-100 border-gray-500 border w-[80%] p-3 mb-1 rounded-xl"/>
-                        <input type="submit" value="SEARCH"/>
-                    </form>
-                    <table className="bg-blue-300 p-1">
-                        <tr>
-                            <td>Name</td>
-                            <td>4Ps Member</td>
-                        </tr>
-                    </table>
+                    
+                    <label htmlFor="search" className="font-medium text-1xl lg:text-2xl">Search</label>
+                    <Input 
+                        type="text" 
+                        placeholder="Seach by name..."
+                        value={searchTerm}
+                        onChange={handleSearch}
+                    />
+                    {/* <ul>
+                        {results.map((item) => (
+                            <li key={item.id}>
+                                {item.name} ({item.age})
+                            </li>
+                        ))}
+                    </ul> */}
+                    {/* <form action="get" className="flex flex-row gap-1">
+                        
+                        <Button className="bg-slate-800 hover:bg-amber-600" type="submit">Search</Button>
+                    </form> */}
+                    <br />
+
+                    <Table className="rounded-md border p-1">
+                        <TableHeader className="border bg-slate-200">
+                            <TableRow className="border">
+                                <TableCell>Respondent ID</TableCell>
+                                <TableCell>Name</TableCell>
+                                <TableCell>Age</TableCell>
+                            </TableRow>
+                        </TableHeader>
+
+                        <TableBody className="border-b-red-500">
+                            {results.map((item) => (
+                                <TableRow key={item.id} className="rounded-md border-b-red-200 ">
+                                    <TableCell>{item.id}</TableCell>
+                                    <TableCell>{item.name}</TableCell>
+                                    <TableCell>{item.age}</TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                    
                 </div>
             </main>
         </div>
--- app/admin/summary/page.tsx
+++ app/admin/summary/page.tsx
@@ -1,36 +1,52 @@
 
+import { ChartContainer } from "@/components/ui/chart"
+import { Separator } from "@/components/ui/separator"
+
+import data from "../../lib/data.json" //mock data
+
 export default function Summary(){
     return(
-        <div>
-            <main>
-                <h1 className="font-black text-3xl py-5 px-2">SUMMARY</h1>
-                <div className="flex flex-wrap align-middle gap-3 p-5">
+        <main className="flex flex-col">
+            <h1 className="font-black text-3xl py-5 px-2">SUMMARY</h1>
+            <Separator/>
+            <div className="flex flex-wrap justify-center gap-2 px-3 py-5">
 
-                    {/* total responses card */}
-                    <div className="bg-slate-300 p-3 rounded-md w-[48%] flex justify-center flex-col">
-                        <h2 className="text-[1rem] lg:text-[2rem] font-medium">TOTAL RESPONSES</h2>
-                        <div className="bg-slate-200 p-2 flex justify-center rounded-1xl">
-                            <p className="font-medium text-1xl lg:text-2xl">12,200</p>
+                <div className="flex flex-col gap-2 ">
+                    <div className="flex flex-row gap-2 w-full">
+                        {/* total responses card */}
+                        <div className="bg-slate-200 p-3 rounded-md flex justify-center flex-col">
+                            <h2 className="text-[1rem] lg:text-[1.5rem] font-medium">Total responses</h2>
+                            <div className="bg-slate-100 p-2 flex justify-center rounded-1xl">
+                                <p className="font-medium text-1xl lg:text-2xl">{data.length}</p>
+                            </div>
+                        </div>
+
+                        {/* second card holder */}
+                        <div className="bg-slate-200 p-3 rounded-md flex justify-center flex-col">
+                            <h2 className="text-[1rem] lg:text-[1.5rem] font-medium">PLACEHOLDER</h2>
+                            <div className="bg-slate-100 p-2 flex justify-center rounded-1xl">
+                                <p className="font-medium text-1xl lg:text-2xl">40%</p>
+                            </div>
+                        </div>
+
+                        {/* thrid card holder */}
+                        <div className="bg-slate-200 p-3 rounded-md flex justify-center flex-col">
+                            <h2 className="text-[1rem] lg:text-[1.5rem] font-medium">PLACEHOLDER</h2>
+                            <div className="bg-slate-100 p-2 flex justify-center rounded-1xl">
+                                <p className="font-medium text-1xl lg:text-2xl">123</p>
+                            </div>
                         </div>
                     </div>
-
-                    {/* second card holder */}
-                    <div className="bg-slate-300 p-3 rounded-md w-[48%] flex justify-center flex-col">
-                        <h2 className="text-[1rem] lg:text-[2rem] font-medium">PLACEHOLDER</h2>
-                        <div className="bg-slate-200 p-2 flex justify-center rounded-1xl">
-                            <p className="font-medium text-1xl lg:text-2xl">12,200</p>
-                        </div>
-                    </div>
-
                     
-
                     {/* charts card */}
-                    <div className="bg-slate-300 p-3 rounded-md w-full">
+                    <div className="bg-slate-200 p-3 rounded-md w-full flex justify-center flex-col">
                         <h2 className="font-bold">Charts</h2>
-                        
+                        <div className="bg-slate-100 p-2 flex justify-center rounded-1xl">
+                            
+                        </div>
                     </div>
                 </div>
-            </main>
-        </div>
+            </div>
+        </main>
     )
 }
