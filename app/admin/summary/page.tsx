36c3a8e86ab382df6f573222eb22792bf6e5543f
marianne-yo <mbalen39@gmail.com>
2026-02-20 14:39:04 +0800

    admin side

--- app/admin/summary/page.tsx
+++ app/admin/summary/page.tsx
@@ -2,14 +2,31 @@
 import { ChartContainer } from "@/components/ui/chart"
 import { Separator } from "@/components/ui/separator"
 
-import data from "../../lib/data.json" //mock data
 import { supabase } from '@/lib/supabase/client'
 
 import { Card, CardDescription, CardHeader, CardTitle, CardAction, CardContent, CardFooter } from "@/components/ui/card"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 export default function Summary(){
-    const [responses, setResponses] = useState<Response[]>([]);
+    const [totalRespondents, setTotalRespondents] = useState(0)
+    const [totalResponses, setTotalResponses] = useState(0)
+
+    useEffect(()=>{
+        const fetchStats = async () => {
+        const { count: respondentCount } = await supabase
+            .from("respondents")
+            .select("*", { count: "exact", head: true })
+
+        const { count: responsesCount } = await supabase
+            .from("responses")
+            .select("*", { count: "exact", head: true })
+
+        setTotalRespondents(respondentCount || 0)
+        setTotalResponses(responsesCount || 0)
+        }
+
+        fetchStats()
+    },[])
 
     return(
         <main className="flex flex-col lg:px-20 sm:p-0 md:px-8">
@@ -19,11 +36,10 @@ export default function Summary(){
 
                 <Card className="lg:w-[20%] sm:w-[30%] md:w-[30%] px-1 bg-slate-50">
                     <CardHeader>
-                        <CardDescription>Total Responses</CardDescription>
+                        <CardDescription>Total Respondents</CardDescription>
                         <CardTitle className="lg:text-4xl font-bold sm:text-3xl md:text-4xl">
-                            
+                            {totalRespondents}
                         </CardTitle>
-                        
                         {/* <CardAction>Card Action</CardAction> */}
                     </CardHeader>
                     <CardContent>
@@ -37,12 +53,12 @@ export default function Summary(){
                 <Card className="lg:w-[20%] sm:w-[30%] md:w-[30%] px-1 bg-slate-50">
                     <CardHeader>
                         <CardDescription>Total Responses</CardDescription>
-                        <CardTitle className="lg:text-4xl font-bold sm:text-3xl md:text-4xl">10,000</CardTitle>
+                        <CardTitle className="lg:text-4xl font-bold sm:text-3xl md:text-4xl">{totalResponses}</CardTitle>
                         
                         {/* <CardAction>Card Action</CardAction> */}
                     </CardHeader>
                     <CardContent>
-                        <p className="text-sm font-light text-gray-500">Total number of responses out of ≈30,000 households</p>
+                        <p className="text-sm font-light text-gray-500">Responses, this counts individual questions, needs more filtering</p>
                     </CardContent>
                     <CardFooter>
                         <p>Card Footer</p>
@@ -78,20 +94,6 @@ export default function Summary(){
                         <p>Card Footer</p>
                     </CardFooter>
                 </Card>
-
-                {/* <div className="flex flex-col gap-2 ">
-                    <div className="flex flex-row gap-2 w-full">
-                       
-                    </div>
-                    
-                    
-                    <div className="bg-slate-200 p-3 rounded-md w-full flex justify-center flex-col">
-                        <h2 className="font-bold">Charts</h2>
-                        <div className="bg-slate-100 p-2 flex justify-center rounded-1xl">
-                            
-                        </div>
-                    </div>
-                </div> */}
             </div>
         </main>
     )
